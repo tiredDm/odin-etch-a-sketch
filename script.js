78e3547b09c9a617e424863b752bdbe4c44f43ca
tiredDm <ronpri1@gmail.com>
2023-09-05 10:32:04 -0400

    Added the prompt for the density button..

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 //create the 16 x 16 grid of divs.. make 16 column divs, with 16 divs inside them that have flex direction of vertical..
-let size  = 16;// doing this incase we want to change it for later..
+let size  = 16;// doing this incase we want to change it for later.. // 1 -> 100
 for(let i = 0; i < size; i++){
     let div = document.createElement('div');
     for(let j = 0; j < size; j++){
@@ -15,4 +15,20 @@ for(let i = 0; i < size; i++){
     container.appendChild(div);
 }
 
-//hover Event listener..    
+// Pixel density button...
+const densityButton = document.querySelector('.density-button');
+densityButton.addEventListener('click', () => {
+    let input;
+    let pd = prompt('Please Enter a Pixel Density:', '64');
+    if(pd == null || pd == '' || !Number(pd)){
+        input = "User did not complete request correctly..";
+    } else {
+        input = pd;
+        changePixelDensity(input);
+    }
+    
+});
+
+function changePixelDensity(pixelDensity){
+    console.log(pixelDensity)
+}
