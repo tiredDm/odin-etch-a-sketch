54d26c7e80a96d57b45f92b7861745eb070038ce
tiredDm <ronpri1@gmail.com>
2023-09-05 11:01:02 -0400

    Added the resizing for pixel density and fixed canvas size..

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 const container = document.querySelector('.container');
 //create the 16 x 16 grid of divs.. make 16 column divs, with 16 divs inside them that have flex direction of vertical..
-let size  = 16;// doing this incase we want to change it for later.. // 1 -> 100
-for(let i = 0; i < size; i++){
-    let div = document.createElement('div');
-    for(let j = 0; j < size; j++){
-        let divChild = document.createElement('div');
-        divChild.classList.add('cell');
-        divChild.addEventListener('mouseover', () => {
-            divChild.style.backgroundColor = "black";
-        });
-        div.appendChild(divChild)
-    }
-    div.classList.add('col');
-    container.appendChild(div);
-}
+// doing this incase we want to change it for later.. // 1 -> 100
+createGrid(16);
+
 
 // Pixel density button...
 const densityButton = document.querySelector('.density-button');
 densityButton.addEventListener('click', () => {
     let input;
     let pd = prompt('Please Enter a Pixel Density:', '64');
-    if(pd == null || pd == '' || !Number(pd)){
+    if(pd == null || pd == '' || !Number(pd) || Number(pd) < 1 || Number(pd) > 100){
         input = "User did not complete request correctly..";
     } else {
         input = pd;
@@ -31,4 +20,30 @@ densityButton.addEventListener('click', () => {
 
 function changePixelDensity(pixelDensity){
     console.log(pixelDensity)
+    //Destroy all divs within "container.." & create the whole thing again..
+    removeGrid();
+    createGrid(pixelDensity);
+}
+
+function createGrid(size){
+    for(let i = 0; i < size; i++){
+        let div = document.createElement('div');
+        for(let j = 0; j < size; j++){
+            let divChild = document.createElement('div');
+            divChild.classList.add('cell');
+            divChild.addEventListener('mouseover', () => {
+                divChild.style.backgroundColor = "black";
+            });
+            div.appendChild(divChild)
+        }
+        div.classList.add('col');
+        container.appendChild(div);
+    }
+}
+
+function removeGrid(){
+    let size = container.childElementCount;
+    for(let i = 0; i < size; i++){
+        container.removeChild(container.firstChild);
+    }
 }
